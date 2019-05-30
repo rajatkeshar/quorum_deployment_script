41a5a1d54fadfe7e683f8562fdd5cfba9e039950
Rajat <rajat@eleven01.io>
2019-05-30 18:32:47 +0530

    join existing raft network

--- raft/join_network.sh
+++ raft/join_network.sh
@@ -14,7 +14,7 @@ function generateKeyPair(){
     cat Key | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//' > pub
     # Extract the private key and remove the leading zero byte
     cat Key | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//' > priv
-     
+
 	rm Key
 }
 
@@ -23,12 +23,12 @@ function copyScripts(){
     cp ../../template/genesis_template_raft.json ../genesis.json
     sed -i "s|#CHAIN_ID#|${chainId}|g" ../genesis.json
     #sed -i "s|#mNodeAddress#|${acc_address}|g" ../genesis.json
-    
-    cp ../../template/tessera_start_template.sh ../tessera_start.sh   
+
+    cp ../../template/tessera_start_template.sh ../tessera_start.sh
     cp ../../template/node_start_template_raft.sh ../node_start.sh
 
     echo ${password} > ../password.txt
-    
+
     sed -i "s/#DPORT/${dPort}/g" ../tessera_start.sh
     sed -i "s/#NODE/'${mNode}'/g" ../tessera_start.sh
     #sed -i "s|#TESSERAJAR|'${tesseraJar}'|g" ../tessera_start.sh
@@ -42,7 +42,7 @@ function copyScripts(){
     then
         #pattern="--permissioned --raftport ${raPort} --rpcport ${rPort} --port ${wPort}"
         pattern="--raftport ${raPort} --rpcport ${rPort} --port ${wPort}"
-    else 
+    else
         pattern="--raftport ${raPort} --rpcport ${rPort} --port ${wPort}"
     fi
     echo "pattern: " $pattern
@@ -118,18 +118,18 @@ function generateEnode(){
 
 #function to create node accout and append it into genesis.json file
 function createAccount(){
-    
+
     echo "[*] Creating account using generated keys and user password"
     # import the private key to geth and create a account
     current_pwd=$(pwd)
 
     acc_address="$(geth account import --datadir ${current_pwd} --password  <(echo $password) priv)"
     re="\{([^}]+)\}"
-    
-    if [[ $acc_address =~ $re ]]; 
+
+    if [[ $acc_address =~ $re ]];
     then
         acc_address="0x"${BASH_REMATCH[1]};
-    fi  
+    fi
     mv keystore/* keystore/key
 }
 
@@ -152,7 +152,7 @@ function executeInit(){
 function generateRaftId(){
 
     nodeId=`sed 's/\[\|\]\|\"'//g static-nodes.json`
-    echo ${nodeId} 
+    echo ${nodeId}
 
     #Add Raft Peers
     result=`curl -X POST "http://${pMainIp}:${pMainPort}" -H "accept: application/json" -H "Content-Type: application/json" --data "{\"jsonrpc\":\"2.0\",\"method\":\"raft_addPeer\",\"params\":[\"${nodeId}\"],\"id\":2}"`
@@ -168,11 +168,11 @@ function generateRaftId(){
     echo "result: " $result
     result=$(echo $result | awk -F ":|}| " '{print $4}')
 	echo "admin_addPeer result: " $result
-    
+
     if [ "$result" = true ];
         then
             echo "peer added successfully!"
-        else 
+        else
             echo "peer not added"
             #exit 1
     fi
@@ -180,12 +180,12 @@ function generateRaftId(){
 
 function createPermissionedJsonFile() {
 
-    result=`curl -X POST "http://${pMainIp}:${pMainPort}" -H "accept: application/json" -H "Content-Type: application/json" --data "{\"jsonrpc\":\"2.0\",\"method\":\"raft_cluster\",\"params\":[],\"id\":2}"`         
+    result=`curl -X POST "http://${pMainIp}:${pMainPort}" -H "accept: application/json" -H "Content-Type: application/json" --data "{\"jsonrpc\":\"2.0\",\"method\":\"raft_cluster\",\"params\":[],\"id\":2}"`
     echo $result
     echo $result | jq '.result'>temp.json
-    
+
     jq -c '.[]' temp.json | while read i; do
-        nodeId=$( echo $i | jq -r '.nodeId' ) 
+        nodeId=$( echo $i | jq -r '.nodeId' )
         raftPort=$( echo $i | jq -r '.raftPort' )
         ip=$( echo $i | jq -r '.ip' )
         p2pPort=$( echo $i | jq -r '.p2pPort' )
@@ -200,17 +200,119 @@ function createPermissionedJsonFile() {
     do
         arr+=(\"$var\")
     done < "$input"
-    
+
     data=$( echo ${arr[@]} | tr ' ' ,)
     echo "[$data]">permissioned-nodes.json
 
     rm temp.json
     rm p1.txt
 }
-function main(){    
+function networkReadParameters() {
+  POSITIONAL=()
+  while [[ $# -gt 0 ]]
+  do
 
-    mNode="$nodeName" 
+      key="$1"
 
+      case $key in
+          --nn|--name)
+          mNode="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --ni|--id)
+          chainId="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --nt|--ntype)
+          nodeType="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --ip)
+          pCurrentIp="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --mip)
+          pMainIp="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --mport)
+          pMainPort="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --r|--rpc)
+          rPort="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --w|--whisper)
+          wPort="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --t|--tessera)
+          tPort="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --dt|--dtessera)
+          dPort="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --raft)
+          raPort="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          --ws)
+          wsPort="$2"
+          shift # past argument
+          shift # past value
+          ;;
+          *)    # unknown option
+          POSITIONAL+=("$1") # save it in an array for later
+          shift # past argument
+          ;;
+      esac
+  done
+  set -- "${POSITIONAL[@]}" # restore positional parameters
+
+  if [[ -z "$mNode" && -z "$chainId" && -z "$nodeType" && -z "$pCurrentIp" && -z "$pMainIp" && -z "$pMainPort" && -z "$rPort" && -z "$wPort" && -z "$tPort" && -z "$raPort" && -z "$dPort" && -z "$wsPort" && -z "$networkName" ]]; then
+      return
+  fi
+
+  if [[ -z "$mNode" || -z "$chainId" || -z "$nodeType" || -z "$pCurrentIp" || -z "$pMainIp" || -z "$pMainPort" || -z "$rPort" || -z "$wPort" || -z "$tPort" || -z "$raPort" || -z "$dPort" || -z "$wsPort" || -z "$networkName" ]]; then
+      help
+  fi
+
+  NETWORK_NON_INTERACTIVE=true
+}
+
+function main(){
+
+    networkReadParameters $@
+
+    if [ -z "$NETWORK_NON_INTERACTIVE" ]; then
+        getInputWithDefault 'Please enter network id' 1101 chainId $BLUE
+        getInputWithDefault 'Please enter node name' "" nodeName $GREEN
+        getInputWithDefault 'Please enter node type permissioned y/n' "y" nodeType $GREEN
+        getInputWithDefault 'Please enter IP Address of main node' "127.0.0.1" pMainIp $RED
+        getInputWithDefault 'Please enter Port of main node' 22000 pMainPort $GREEN
+        getInputWithDefault 'Please enter IP Address of this node' "127.0.0.1" pCurrentIp $RED
+        getInputWithDefault 'Please enter RPC Port of this node' 22000 rPort $GREEN
+        getInputWithDefault 'Please enter Network Listening Port of this node' $((rPort+1)) wPort $GREEN
+        getInputWithDefault 'Please enter Tessera Port of this node' $((wPort+1)) tPort $GREEN
+        getInputWithDefault 'Please enter Tessera debug Port of this node' $((tPort+1)) dPort $GREEN
+        getInputWithDefault 'Please enter Raft Port of this node' $((dPort+1)) raPort $PINK
+        getInputWithDefault 'Please enter WS Port of this node' $((raPort+1)) wsPort $GREEN
+    fi
+    mNode="$nodeName"
     cleanup
     generateKeyPair
     createAccount
@@ -220,7 +322,7 @@ function main(){
     generateTesseraConfig
     generateRaftId
     #createPermissionedJsonFile
-    executeInit   
+    executeInit
 }
 
 main $@
